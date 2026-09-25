448a58333fae10fbbc653c71d3325c58fc235fad
Vũ Trọng Nghĩa <2411060124@hunre.edu.vn>
2026-09-25 08:20:18 +0700

    Hoan thanh buoi 6

---
+++ Buoi6/main.py
@@ -0,0 +1,25 @@
+import utils
+
+
+print("Dao nguoc chuoi:")
+print(utils.dao_nguoc_chuoi("Python"))
+
+
+print("\nKiem tra palindrome:")
+print(utils.kiem_tra_palindrome("madam"))
+
+
+print("\nChuan hoa ho ten:")
+print(
+    utils.chuan_hoa_ho_ten(
+        " nguyen van an "
+    )
+)
+
+
+print("\nUSCLN:")
+print(utils.uscln(24, 36))
+
+
+print("\nKiem tra so nguyen to:")
+print(utils.kiem_tra_nguyen_to(29))
